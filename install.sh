7067f474be7e4e5d4ae34ad0b55098cbd82e4b26
Li Hua <lihua@email.com>
2024-08-12 10:17:54 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 pacman-key --init
 pacman-key --populate archlinux
 pacman-key --lsign-key  "farseerfc@archlinux.org" 
-archinstall --config https://cdn.jsdelivr.net/gh/real-LiHua/dotfiles/user_configuration.json
+archinstall --config https://cdn.jsdelivr.net/gh/real-LiHua/dotfiles/user_configuration.json --silent --script guided
