c2f0e7dee614614e6634d75e3ad0ac7f94a697c8
Li Hua <lihua@email.com>
2024-08-13 02:28:45 +0800

    Update
    Signed-off-by: Li Hua <lihua@email.com>

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 pacman -Sy --noconfirm archlinux-keyring
-pacman-key --lsign-key "farseerfc@archlinux.org"
+#pacman-key --lsign-key "farseerfc@archlinux.org"
 archinstall \
     --config https://raw.githubusercontent.com/real-LiHua/dotfiles/main/user_configuration.json \
     --silent \
