8e43072da4f381d5d64d37d6dd86896e81f01643
Li Hua <lihua@email.com>
2024-08-13 12:20:43 +0800

    Update
    Signed-off-by: Li Hua <lihua@email.com>

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #pacman -Sy --needed --noconfirm archlinux-keyring
 #pacman-key --lsign-key "farseerfc@archlinux.org"
+sed -i '/.* raw.githubusercontent.com/d' /etc/hosts
+for ip in 185.199.{108..111}.133; do
+    curl --connect-timeout 1 \
+         --resolve raw.githubusercontent.com:443:$ip
+         -sLo /dev/null https://raw.githubusercontent.com && \
+    echo "$ip raw.githubusercontent.com" | tee -a /etc/hosts && break
+done
+
 archinstall \
     --config https://raw.githubusercontent.com/real-LiHua/dotfiles/main/user_configuration.json \
     --silent \
