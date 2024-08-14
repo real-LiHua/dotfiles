54876910ac751e19850de8c2679424eaa595d927
Li Hua <lihua@email.com>
2024-08-15 07:48:36 +0800

    Update
    Signed-off-by: Li Hua <lihua@email.com>

--- pre_add.py
+++
@@ -1,8 +0,0 @@
-# /usr/bin/python3
-from os import getenv
-from os.path import abspath
-from pathlib import Path
-
-print(getenv("CHEZMOI_WORKING_TREE"))
-for path in getenv("CHEZMOI_ARGS", "").split()[2:]:
-   print(abspath(Path(abspath(path)) / ".."))
