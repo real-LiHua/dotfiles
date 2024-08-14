cfe2abb3e60e21fea045f732ff18c646acfb2e26
Li Hua <lihua@email.com>
2024-08-15 07:54:57 +0800

    Update
    Signed-off-by: Li Hua <lihua@email.com>

--- hook.py
+++ hook.py
@@ -4,7 +4,12 @@ from os import getenv
 from os.path import abspath
 from pathlib import Path
 
-print(environ)
-print(getenv("CHEZMOI_WORKING_TREE"))
-for path in getenv("CHEZMOI_ARGS", "").split()[2:]:
-   print(abspath(Path(abspath(path)) / ".."))
+match getenv("CHEZMOI_COMMAND"):
+    case "apply":
+        print(Path(getenv("CHEZMOI_WORKING_TREE")) / "root")
+    case "add":
+        pass
+    case _:
+        print(environ)
+#for path in getenv("CHEZMOI_ARGS", "").split()[2:]:
+#   print(abspath(Path(abspath(path)) / ".."))
