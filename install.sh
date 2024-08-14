7db62eccacc730076df461cb5392a7e3cbb2cd56
Li Hua <lihua@email.com>
2024-08-14 11:35:16 +0800

    Update
    Signed-off-by: Li Hua <lihua@email.com>

--- install.sh
+++ install.sh
@@ -9,14 +9,14 @@ parted -sa  opt "$disk"             \
     set     2       root    on
 mkfs.fat -vF 32 "$disk"1
 mkfs.btrfs -vfn 32k "$disk"2
-mount -vo compress=lzo "$disk"2 /mnt
-echo -n ,home,opt,usr,var | xargs -i -d, btrfs -v subvolume create /mnt/@{}
-umount -v /mnt
-mount  -vo noatime,nodiratime,compress=lzo,subvol=@ "$disk"2 /mnt
-mkdir  -v /mnt/{boot,home,opt,usr,var}
-mount  -vo noatime,nodiratime "$disk"1 /mnt/boot
+mount -vo compress=lzo "$disk"2 /mnt/arch_btrfs
+echo -n ,home,opt,usr,var | xargs -i -d, btrfs -v subvolume create /mnt/arch_btrfs/@{}
+umount -v /mnt/arch_btrfs
+mount  -vo noatime,nodiratime,compress=lzo,subvol=@ "$disk"2 /mnt/archinstall
+mkdir  -v /mnt/archinstall/{boot,home,opt,usr,var}
+mount  -vo noatime,nodiratime "$disk"1 /mnt/archinstall/boot
 echo -n  home,opt,usr,var | xargs -i -d, \
-    mount -vo noatime,nodiratime,compress=lzo,subvol=@{} "$disk"2 /mnt/{}
+    mount -vo noatime,nodiratime,compress=lzo,subvol=@{} "$disk"2 /mnt/archinstall/{}
 chattr +C /mnt/var
 archinstall \
     --config https://raw.githubusercontent.com/real-LiHua/dotfiles/main/user_configuration.json \
