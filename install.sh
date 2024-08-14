0342d9efbbaef8c9c9365b795fd63c611fa56cd4
Li Hua <lihua@email.com>
2024-08-14 14:09:44 +0800

    Update
    Signed-off-by: Li Hua <lihua@email.com>

--- install.sh
+++ install.sh
@@ -8,17 +8,16 @@ parted -sa  opt "$disk"             \
     set     2       root    on
 mkfs.fat -vF 32 "$disk"1
 mkfs.btrfs -vfn 32k "$disk"2
-mkdir -v /mnt/archinstall
-mount -vo compress=zstd "$disk"2 /mnt/archinstall
-echo -n ,home,log,pkg,.snapshots | xargs -i -d, btrfs -v subvolume create /mnt/archinstall/@{}
-umount -vR /mnt/archinstall
-mount  -vo compress=zstd,subvol=@ "$disk"2 /mnt/archinstall
-mkdir  -vp /mnt/archinstall/{boot,home,var/log,var/cache/pacman/pkg,.snapshots}
-mount  -v "$disk"1 /mnt/archinstall/boot
-mount -vo compress=zstd,subvol=@home       "$disk"2 /mnt/archinstall/home
-mount -vo compress=zstd,subvol=@log        "$disk"2 /mnt/archinstall/var/log
-mount -vo compress=zstd,subvol=@pkg        "$disk"2 /mnt/archinstall/var/cache/pacman/pkg
-mount -vo compress=zstd,subvol=@.snapshots "$disk"2 /mnt/archinstall/.snapshots
+mount -vo compress=zstd "$disk"2 /mnt
+echo -n ,home,log,pkg,.snapshots | xargs -i -d, btrfs -v subvolume create /mnt/@{}
+umount -vR /mnt
+mount -vo compress=zstd,subvol=@           "$disk"2 /mnt
+mkdir -vp /mnt/{boot,home,var/log,var/cache/pacman/pkg,.snapshots}
+mount -v "$disk"1 /mnt/boot
+mount -vo compress=zstd,subvol=@home       "$disk"2 /mnt/home
+mount -vo compress=zstd,subvol=@log        "$disk"2 /mnt/var/log
+mount -vo compress=zstd,subvol=@pkg        "$disk"2 /mnt/var/cache/pacman/pkg
+mount -vo compress=zstd,subvol=@.snapshots "$disk"2 /mnt/.snapshots
 archinstall \
     --config https://raw.githubusercontent.com/real-LiHua/dotfiles/main/user_configuration.json \
     --creds https://raw.githubusercontent.com/real-LiHua/dotfiles/main/user_credentials.json \
