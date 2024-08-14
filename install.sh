311a04a8452f421911ebc08eab8896936fc1cc7b
Li Hua <lihua@email.com>
2024-08-14 21:22:06 +0800

    Update
    Signed-off-by: Li Hua <lihua@email.com>

--- install.sh
+++ install.sh
@@ -6,11 +6,14 @@ parted -sa  opt "$disk"             \
     mkpart  primary btrfs   1G 100% \
     set     1       esp     on      \
     set     2       root    on
+
 mkfs.fat -vF 32 "$disk"1
 mkfs.btrfs -vfn 32k "$disk"2
+
 mount -vo compress=zstd "$disk"2 /mnt
 echo -n ,home,log,pkg,.snapshots | xargs -i -d, btrfs -v subvolume create /mnt/@{}
 umount -vR /mnt
+
 mount -vo compress=zstd,subvol=@           "$disk"2 /mnt
 mkdir -vp /mnt/{boot,home,var/log,var/cache/pacman/pkg,.snapshots}
 mount -v "$disk"1 /mnt/boot
@@ -18,6 +21,9 @@ mount -vo compress=zstd,subvol=@home       "$disk"2 /mnt/home
 mount -vo compress=zstd,subvol=@log        "$disk"2 /mnt/var/log
 mount -vo compress=zstd,subvol=@pkg        "$disk"2 /mnt/var/cache/pacman/pkg
 mount -vo compress=zstd,subvol=@.snapshots "$disk"2 /mnt/.snapshots
+
+pacman-key -r 4345771566D76038C7FEB43863EC0ADBEA87E4E3
+
 archinstall \
     --config https://raw.githubusercontent.com/real-LiHua/dotfiles/main/user_configuration.json \
     --creds https://raw.githubusercontent.com/real-LiHua/dotfiles/main/user_credentials.json \
