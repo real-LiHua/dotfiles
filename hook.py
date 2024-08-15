bd704220a629c0b65d6caaceae28fd940fcf10e3
Li Hua <lihua@email.com>
2024-08-15 08:25:47 +0800

    Update
    Signed-off-by: Li Hua <lihua@email.com>

--- hook.py
+++ hook.py
@@ -1,12 +1,16 @@
-# /usr/bin/python3
-from os import environ
-from os import getenv
+#!/usr/bin/python3
+from os import environ, getenv, getuid
 from os.path import abspath
 from pathlib import Path
+from shutil import copytree, ignore_patterns
+from subprocess import run
 
 match getenv("CHEZMOI_COMMAND"):
     case "apply":
-        print(Path(getenv("CHEZMOI_WORKING_TREE")) / "root")
+        if getuid() != 0:
+            run(["sudo", "-E", "/usr/bin/python3", __file__])
+            exit()
+        copytree(Path(getenv("CHEZMOI_WORKING_TREE")) / "root", "/", ignore=ignore_patterns("*~"), dirs_exist_ok=True)
     case "add":
         pass
     case _:
