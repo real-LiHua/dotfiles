ac3dfa539a7b2c5ec49d908b277577c33aa27ba6
Li Hua <lihua@email.com>
2024-08-16 11:57:25 +0800

    Update
    Signed-off-by: Li Hua <lihua@email.com>

--- hook.py
+++ hook.py
@@ -1,15 +1,33 @@
 #!/usr/bin/python3
+from filecmp import dircmp
 from os import environ, getenv, getuid
 from os.path import abspath
 from pathlib import Path
 from shutil import copytree, ignore_patterns
 from subprocess import run
 
+
+def has_diff(diff: dircmp) -> bool:
+    if diff.diff_files:
+        return True
+    for sub in diff.subdirs.values():
+        if has_diff(sub):
+            return True
+    return False
+
 def apply():
+    source: Path = Path(getenv("CHEZMOI_WORKING_TREE")) / "root"
+    target: str = "/"
+    try:
+        if not has_diff(dircmp(source, target)):
+            return
+    except RecursionError:
+        pass
     if getuid():
         run(["sudo", "-E", "/usr/bin/python3", __file__])
         return
-    copytree(Path(getenv("CHEZMOI_WORKING_TREE")) / "root", "/", ignore=ignore_patterns("*~"), dirs_exist_ok=True)
+    ignore = ignore_patterns("*~")
+    copytree(source, target, ignore=ignore, dirs_exist_ok=True)
 
 match getenv("CHEZMOI_COMMAND"):
     case "update" | "apply":
