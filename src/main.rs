22d8a46877e5e5caa457c1f7ec486b70c7ee90e1
Li Hua <lihua@email.com>
2024-09-01 22:42:23 +0800

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::env::var;
+use std::string::String;
+use uzers::get_current_uid;
 fn main() {
-    println!("Hello, world!");
+    if get_current_uid() == 0 {
+    }
+    let _init = String::from("init");
+    match var("HOME") {
+        _init => println!("1"),
+
+    }
 }
