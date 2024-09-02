69a1a35bf51b871933f620fd85c57643379fb91f
Li Hua <lihua@email.com>
2024-09-02 08:25:44 +0800

    Update

--- home/dot_config/nvim/lua/plugins/xvzc@chezmoi.nvim.lua
+++ home/dot_config/nvim/lua/plugins/xvzc@chezmoi.nvim.lua
@@ -1,18 +1,18 @@
 return {
-		"xvzc/chezmoi.nvim",
-		lazy = false,
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			require("chezmoi").setup({
-				edit = {
-					watch = true,
-				},
-				notification = {
-					on_watch = true,
-				},
-				telescope = {
-					select = { "<CR>" },
-				},
-			})
-		end,
-	}
+	"xvzc/chezmoi.nvim",
+	lazy = false,
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		require("chezmoi").setup({
+			edit = {
+				watch = true,
+			},
+			notification = {
+				on_watch = true,
+			},
+			telescope = {
+				select = { "<CR>" },
+			},
+		})
+	end,
+}
