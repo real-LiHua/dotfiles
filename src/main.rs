5bd631dab3cd80a60da171cdfe26dee944e97de1
Li Hua <lihua@email.com>
2024-09-02 08:16:37 +0800

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use std::env::var;
 use std::string::String;
 use uzers::get_current_uid;
+// TODO: 在命令执行前改变环境变量应该可以改变 chezmoi 的逻辑
+// 现有的 hook.py 感觉不太优雅（也许应该写 shell 脚本（
 fn main() {
-    if get_current_uid() == 0 {
-    }
+    if get_current_uid() == 0 {}
     let _init = String::from("init");
     match var("HOME") {
         _init => println!("1"),
     }
 }
-
