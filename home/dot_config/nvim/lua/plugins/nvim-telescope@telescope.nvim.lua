9837fbd4641334af2c3fd96583286aacecb96656
Li Hua <lihua@email.com>
2024-09-02 08:03:40 +0800

    Update

---
+++ home/dot_config/nvim/lua/plugins/nvim-telescope@telescope.nvim.lua
@@ -0,0 +1,11 @@
+return {
+	"nvim-telescope/telescope.nvim",
+	lazy = false,
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"BurntSushi/ripgrep",
+		"nvim-telescope/telescope-fzf-native.nvim",
+		"nvim-treesitter/nvim-treesitter",
+		"nvim-tree/nvim-web-devicons",
+	}
+}
--- home/dot_config/nvim/lua/plugins/nvim-treesitter@nvim-treesitter.lua
+++ home/dot_config/nvim/lua/plugins/nvim-treesitter@nvim-treesitter.lua
@@ -1,6 +1,5 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	lazy = false,
 	build = ":TSUpdate",
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter-context",
